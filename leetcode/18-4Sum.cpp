b928fcb55b4ce48dc859d994854820960e05344f
HUSTLX <1508338149@qq.com>
2016-03-07 15:46:18 +0800

    4sum

--- leetcode/18-4Sum.cpp
+++ leetcode/18-4Sum.cpp
@@ -9,17 +9,17 @@ vector<vector<int>> fourSum(vector<int>& nums, int target) {
     vector<vector<int>> res;
     sort(nums.begin(), nums.end());
     int len = nums.size();
-    
+
     for (int i = 0; i < len; i++) {
         for (int j = i + 1; j < len; j++) {
             int third = j + 1, fouth = len - 1;
             while (third < fouth) {
-                int sum = nums[i]+nums[j]+ nums[third]+nums[fouth];
+                int sum = nums[i] + nums[j] + nums[third] + nums[fouth];
                 if (target == sum) {
                     vector<int> temp = { nums[i],nums[j],nums[third],nums[fouth] };
                     res.push_back(temp);
-                    while (third < fouth && nums[third ] == temp[2]) third++;
-                    while (third < fouth && nums[fouth ] == temp[3]) fouth--;
+                    while (third < fouth && nums[third] == temp[2]) third++;
+                    while (third < fouth && nums[fouth] == temp[3]) fouth--;
                 }
                 else sum < target ? third++ : fouth--;
             }
@@ -30,28 +30,7 @@ vector<vector<int>> fourSum(vector<int>& nums, int target) {
     return res;
 }
 int main() {
-    vector<int> nums = { 1,0,-1,0,-2,2};
+    vector<int> nums = { 1,0,-1,0,-2,2 };
     int target = 0;
-    vector<vector<int>> res=fourSum(nums, target);
+    vector<vector<int>> res = fourSum(nums, target);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
